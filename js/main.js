93d1a5d3dac3c33ff6a432af9fb8fca06a89e559
Emily Garvey <emilygarvey@MacBook-Air-7.local>
2019-02-19 18:03:55 -0500

    with map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), 
-	{
-		center: {lat: 40.8054491, lng: -73.9654415},
-		zoom: 15
-	});
+  console.log("hello rory")
+  console.log(google)
+  var map = new google.maps.Map(document.getElementById('map'), 
+  {
+    center: {lat: 40.8054491, lng: -73.9654415},
+    zoom: 15
+  });
+  var marker = new google.maps.Marker({
+          position: {lat: 40.8054491, lng: -73.9654415},
+          map: map,
+          title: 'Eat here!'
+        });
 }
+
+document.onload = initMap;
